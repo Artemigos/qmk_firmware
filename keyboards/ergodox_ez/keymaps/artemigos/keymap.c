667ffe76c4303d53cebb3d7e440df9e9a9facaaf
Łukasz Klimek <klinn91@gmail.com>
2020-07-14 20:57:33 +0200

    simplified layout code + minor tweaks

--- keyboards/ergodox_ez/keymaps/artemigos/keymap.c
+++ keyboards/ergodox_ez/keymaps/artemigos/keymap.c
@@ -4,142 +4,14 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
+#include "dynamic_macro.h"
 
 /**
  * See `readme.md` for notes on each define.
  */
 
-/**
- * Optionally support 80 key layout.
- *
- * No default keys defined: use 'CFQ_USER_K80_L0K0' .. etc.
- */
-/* #define CFQ_USE_80_KEYS */
-
-/** Personal preference (enable by passing 'EXTRAFLAGS=...' to make). */
-/* #define CFQ_USE_MOMENTARY_LAYER_KEYS */
-
 /** Holding right/left or left/right shift for single or double quote pair */
 /* #define CFQ_USE_SHIFT_QUOTES */
-
-#define CFQ_USE_DYNAMIC_MACRO
-
-#if !defined(CFQ_USER_KEY0)
-#  define CFQ_USER_KEY0 KC_APP
-#endif
-#if !defined(CFQ_USER_KEY1)
-#  define CFQ_USER_KEY1 KC_MENU
-#endif
-#if !defined(CFQ_USER_KEY2)
-#  define CFQ_USER_KEY2 KC_BSPC
-#endif
-#if !defined(CFQ_USER_KEY3)
-#  define CFQ_USER_KEY3 KC_DEL
-#endif
-#if !defined(CFQ_USER_KEY4)
-#  define CFQ_USER_KEY4 KC_SPC
-#endif
-#if !defined(CFQ_USER_KEY5)
-#  define CFQ_USER_KEY5 CFQ_KC_FN1
-#endif
-#if !defined(CFQ_USER_KEY6)
-#  define CFQ_USER_KEY6 KC_CAPS
-#endif
-#if !defined(CFQ_USER_KEY7)
-#  define CFQ_USER_KEY7 KC_INS
-#endif
-#if !defined(CFQ_USER_KEY8)
-#  define CFQ_USER_KEY8 KC_DEL
-#endif
-#if !defined(CFQ_USER_KEY9)
-#  define CFQ_USER_KEY9 KC_BSPC
-#endif
-
-
-#ifdef CFQ_USE_80_KEYS
-#  define LAYOUT_ergodox_76_or_80 LAYOUT_ergodox_80
-#  define K80(a) CFQ_USER_K80_##a
-#else
-#  define LAYOUT_ergodox_76_or_80( \
-        k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-        k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-        k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
-        k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, \
-         \
-        k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, \
-        k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, \
-        k60, k61, k62, k63, k64, k65, k66, k67, k68, k69, \
-        k70, k71, k72, k73, k74, k75, k76, k77, k78, k79) \
-  LAYOUT_ergodox( \
-          k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-          k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-          k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
-          k30, k31, k32, k33,           k36, k37, k38, k39, \
-           \
-          k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, \
-          k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, \
-          k60, k61, k62, k63, k64, k65, k66, k67, k68, k69, \
-          k70, k71, k72, k73, k74,           k77, k78, k79)
-#  define K80(a) KC_TRNS
-#endif
-
-/**
- * Used to generate lines below:
- * \code{.py}
- * text = '#  ifndef CFQ_USER_K80_L0K0\n#    define CFQ_USER_K80_L0K0 KC_TRNS\n#  endif'
- * print('\n'.join([text.replace('L0', f'L{l}').replace('K0', f'K{k}') for l in range(3) for k in range(4)]))
- * \endcode
- */
-#ifdef CFQ_USE_80_KEYS
-#  ifndef CFQ_USER_K80_L0K0
-#    define CFQ_USER_K80_L0K0 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L0K1
-#    define CFQ_USER_K80_L0K1 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L0K2
-#    define CFQ_USER_K80_L0K2 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L0K3
-#    define CFQ_USER_K80_L0K3 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L1K0
-#    define CFQ_USER_K80_L1K0 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L1K1
-#    define CFQ_USER_K80_L1K1 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L1K2
-#    define CFQ_USER_K80_L1K2 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L1K3
-#    define CFQ_USER_K80_L1K3 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L2K0
-#    define CFQ_USER_K80_L2K0 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L2K1
-#    define CFQ_USER_K80_L2K1 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L2K2
-#    define CFQ_USER_K80_L2K2 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L2K3
-#    define CFQ_USER_K80_L2K3 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L3K0
-#    define CFQ_USER_K80_L3K0 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L3K1
-#    define CFQ_USER_K80_L3K1 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L3K2
-#    define CFQ_USER_K80_L3K2 KC_TRNS
-#  endif
-#  ifndef CFQ_USER_K80_L3K3
-#    define CFQ_USER_K80_L3K3 KC_TRNS
-#  endif
-#endif
 
 #ifndef CFQ_WORD_A
 #define CFQ_WORD_A ""
@@ -273,31 +145,12 @@ enum custom_keycodes {
   M_WORD_S, M_WORD_T, M_WORD_U, M_WORD_V, M_WORD_W, M_WORD_X,
   M_WORD_Y, M_WORD_Z,
 
-#ifdef CFQ_USE_DYNAMIC_MACRO
   DYNAMIC_MACRO_RANGE,
-#endif
 };
 
-#ifdef CFQ_USE_DYNAMIC_MACRO
-#  include "dynamic_macro.h"
-#else
-   /* avoid ifdef's in keymap */
-#  define DYN_REC_START1 KC_TRNS
-#  define DYN_REC_START2 KC_TRNS
-#  define DYN_MACRO_PLAY1 KC_TRNS
-#  define DYN_MACRO_PLAY2 KC_TRNS
-#  define DYN_REC_STOP KC_TRNS
-#endif
-
-#ifdef CFQ_USE_MOMENTARY_LAYER_KEYS
 #define CFQ_KC_FN1 MO(1)
 #define CFQ_KC_FN2 MO(2)
 #define CFQ_KC_FN3 MO(3)
-#else
-#define CFQ_KC_FN1 TT(1)
-#define CFQ_KC_FN2 TT(2)
-#define CFQ_KC_FN3 TT(3)
-#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -310,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   [  |  |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |  |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   | LCtl |Super | Alt  | App  | Menu |                              | Left | Down | Up   |Right | Del  |
+ *   | LCtl |Super | Alt  | App  | Menu |                              | Left | Down | Up   |Right |Hyper |
  *   '----------------------------------'                              '----------------------------------'
  *                                      .-------------.  .-------------.
  *                                      |BSpace| Del  |  | Home | End  |
@@ -319,48 +172,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |Space | ~L1  |------|  |------| ~L2  |Enter |
  *                               |      |      |Insert|  | PgDn |      |      |
  *                               '--------------------'  '--------------------'
- *
- * Optional overrides: see CFQ_USER_KEY# defines.
- * .--------------------------------------------------.  .--------------------------------------------------.
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      | USR9   |
- * |--------+------+------+------+------+------+------|  |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|  |------|      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      |        |
- * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   |      |      |      | USR0 | USR1 |                              |      |      |      |      | USR8 |
- *   '----------------------------------'                              '----------------------------------'
- *                                      .-------------.  .-------------.
- *                                      | USR2 | USR3 |  |      |      |
- *                               .------+------+------|  |------+------+------.
- *                               |      |      | USR6 |  |      |      |      |
- *                               | USR4 | USR5 |------|  |------|      |      |
- *                               |      |      | USR7 |  |      |      |      |
- *                               '--------------------'  '--------------------'
  */
 
 /* If it accepts an argument (i.e, is a function), it doesn't need KC_.
  * Otherwise, it needs KC_* */
-[LAYER_BASE] = LAYOUT_ergodox_76_or_80(  /* layer 0 : default */
+[LAYER_BASE] = LAYOUT_ergodox(  /* layer 0 : default */
   /* left hand */
-  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH,       KC_DLR, KC_PERC, KC_LCBR,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,          KC_R,   KC_T,    KC_LPRN,
-  KC_ESC,  KC_A,    KC_S,    KC_D,          KC_F,   KC_G,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,   KC_B,    KC_LBRC,
-  KC_LCTL, KC_LGUI, KC_LALT, CFQ_USER_KEY0, CFQ_USER_KEY1,
-                                                    CFQ_USER_KEY2, CFQ_USER_KEY3,
-                                     K80(L0K0),     K80(L0K1),     CFQ_USER_KEY6,
-                                     CFQ_USER_KEY4, CFQ_USER_KEY5, CFQ_USER_KEY7,
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC, KC_LCBR,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_LPRN,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_LBRC,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_APP,  KC_MENU,
+                                              KC_BSPC, KC_DEL,
+                                                       KC_CAPS,
+                                   KC_SPC, CFQ_KC_FN1, KC_INS,
   /* right hand */
-  KC_RCBR,     KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS, KC_EQL,    CFQ_USER_KEY9,
+  KC_RCBR,     KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS, KC_EQL,    KC_BSPC,
   KC_RPRN,     KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,      KC_BSLS,
                KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN,   KC_QUOT,
   KC_RBRC,     KC_N,    KC_M,    KC_COMM,KC_DOT,  KC_SLSH,   KC_RSFT,
-                        KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT,   CFQ_USER_KEY8,
+                        KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT,   KC_HYPR,
   KC_HOME, KC_END,
-  KC_PGUP, K80(L0K2),  K80(L0K3),
+  KC_PGUP,
   KC_PGDN, CFQ_KC_FN2, KC_ENT
 ),
 /* Keymap 1: Keypad, Bracket Pairs & Macro Record
@@ -384,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |      |      |Play2 |  |      |      |      |
  *                               '--------------------'  '--------------------'
  */
-[LAYER_KPAD] = LAYOUT_ergodox_76_or_80(
+[LAYER_KPAD] = LAYOUT_ergodox(
   /* left hand */
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          M_BRACKET_IN_CBR,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_ARROW_REQL,     M_BRACKET_IN_PRN,
@@ -392,7 +225,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_BRACKET_IN_ANG, M_BRACKET_IN_BRC,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                DYN_REC_START1,   DYN_REC_START2,
-                              K80(L1K0),       K80(L1K1),        DYN_MACRO_PLAY1,
+                                                                 DYN_MACRO_PLAY1,
                               DYN_REC_STOP,    KC_TRNS,          DYN_MACRO_PLAY2,
   /* right hand */
   M_BRACKET_OUT_CBR, KC_TRNS,           KC_NLCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,  KC_TRNS,
@@ -401,7 +234,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   M_BRACKET_OUT_BRC, M_BRACKET_OUT_ANG, KC_KP_1, KC_KP_2,     KC_KP_3,        KC_KP_ENTER, KC_TRNS,
                                         KC_KP_0, KC_TRNS,     KC_KP_DOT,      KC_KP_ENTER, KC_TRNS,
   KC_TRNS, KC_TRNS,
-  KC_TRNS, K80(L1K2), K80(L1K3),
+  KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Function Keys, Media & Mouse Keys
@@ -425,7 +258,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |      |      |      |  | MNxt |      |      |
  *                               '--------------------'  '--------------------'
  */
-[LAYER_FKEY] = LAYOUT_ergodox_76_or_80(
+[LAYER_FKEY] = LAYOUT_ergodox(
   /* left hand */
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_WH_U,
@@ -433,7 +266,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_BTN2, KC_BTN3, KC_BTN1, KC_TRNS, KC_WH_D,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                       KC_TRNS,   KC_TRNS,
-                           K80(L2K0), K80(L2K1), KC_TRNS,
+                                                 KC_TRNS,
                            KC_TRNS,   KC_TRNS,   KC_TRNS,
   /* right hand */
   KC_MUTE,  KC_TRNS, KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
@@ -442,7 +275,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_VOLD,  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_TRNS, KC_TRNS,
                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_MRWD, KC_MFFD,
-  KC_MPRV, K80(L2K2), K80(L2K3),
+  KC_MPRV,
   KC_MNXT, KC_TRNS, KC_MPLY
 ),
 /* Keymap 3: Entire Words (one for each key) & Numbers
@@ -458,7 +291,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
  * |        |   Z  |   X  |   C  |   V  |   B  |      |  |      |   N  |   M  |      |      |      |        |
  * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                              |      |      |      |      |      |
+ *   |      |      |      |      |      |                              |      |      |      |      |Reset |
  *   '----------------------------------'                              '----------------------------------'
  *                                      .-------------.  .-------------.
  *                                      |      |      |  |      |      |
@@ -468,7 +301,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |      |      |      |  |      |      |      |
  *                               '--------------------'  '--------------------'
  */
-[LAYER_WORD] = LAYOUT_ergodox_76_or_80(
+[LAYER_WORD] = LAYOUT_ergodox(
   /* left hand */
   KC_TRNS, KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,
   KC_TRNS, M_WORD_Q, M_WORD_W, M_WORD_E, M_WORD_R, M_WORD_T, KC_TRNS,
@@ -476,16 +309,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, M_WORD_Z, M_WORD_X, M_WORD_C, M_WORD_V, M_WORD_B, KC_TRNS,
   KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                     KC_TRNS,   KC_TRNS,
-                                         K80(L3K0), K80(L3K1), KC_TRNS,
+                                                               KC_TRNS,
                                          KC_TRNS,   KC_TRNS,   KC_TRNS,
   /* right hand */
   KC_TRNS, KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,
   KC_TRNS, M_WORD_Y, M_WORD_U, M_WORD_I, M_WORD_O, M_WORD_P, KC_TRNS,
            M_WORD_H, M_WORD_J, M_WORD_K, M_WORD_L, KC_TRNS,  KC_TRNS,
   KC_TRNS, M_WORD_N, M_WORD_M, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET,
   KC_TRNS, KC_TRNS,
-  KC_TRNS, K80(L3K2), K80(L3K3),
+  KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS
 ),
 };
@@ -505,11 +338,9 @@ uint32_t layer_state_set_user(uint32_t state) {
   } while (0)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef CFQ_USE_DYNAMIC_MACRO
   if (!process_record_dynamic_macro(keycode, record)) {
     return false;
   }
-#endif
   switch (keycode) {
     /* dynamically generate these. */
     case RGB_SLD:
